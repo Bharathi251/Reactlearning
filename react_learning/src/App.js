72ceb3a3e14fb0173a627b20127d773801d9730e
Bharathi251 <bharathirajkumar251@gmail.com>
2023-09-12 13:03:24 +0530

    first commit

---
+++ react_learning/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <p>
+      Hai I am Bharathi
+    </p>
+
+    </div>
+
+
+
+
+  );
+}
+
+export default App;
